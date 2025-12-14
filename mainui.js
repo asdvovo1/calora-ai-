d9fb7fb298a6310b8c22ad092a2fbc8000743812
asdvovo1 <82127997+asdvovo1@users.noreply.github.com>
2025-12-14 14:28:20 +0000

    Update code

--- mainui.js
+++ mainui.js
@@ -506,7 +506,7 @@ const MagicLineTabBar = ({ state, descriptors, navigation, theme, t, language })
 
     const getTabPosition = (index) => {
         const positions = isRTL 
-            ? [-3, -2, -1, 0]  
+            ? [0, 1, 2, 0]  
             : [0, -1, -2, -3]; 
         
         return positions[index] * TAB_WIDTH;
